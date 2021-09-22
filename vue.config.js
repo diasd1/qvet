0bd019ffdfb6242905a0c71a06af94f9b3164e88
dxstiny <48208462+dxstiny@users.noreply.github.com>
2021-09-22 21:31:54 +0200

    updated favicon

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/'
-      : '/'
+  pwa: {
+    iconPaths: {
+      favicon32: "./public/favicon.png",
+      favicon16: "./public/favicon.png",
+      appleTouchIcon: "./public/favicon.png",
+      maskIcon: "./public/favicon.png",
+      msTileImage: "./public/favicon.png"
+    }
   }
+};
