0fd9db2c1cfa449242c0a1f4884a454b4ce6af92
dxstiny <48208462+dxstiny@users.noreply.github.com>
2021-09-23 18:25:14 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -13,13 +13,13 @@ module.exports = {
         appleMobileWebAppCapable: 'yes',
         appleMobileWebAppStatusBarStyle: 'black',
 
-        // configure the workbox plugin
+        /*// configure the workbox plugin
         workboxPluginMode: 'InjectManifest',
         workboxOptions: {
             // swSrc is required in InjectManifest mode.
             swSrc: 'dev/sw.js',
             // ...other Workbox options...
-        }
+        }*/
     },
     configureWebpack: {
         performance: {
