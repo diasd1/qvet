7caadc23d5b3ee660e37d1289d47397133a28679
dxstiny <david.diashorta@gmail.com>
2021-12-06 20:48:13 +0100

    0.9.7 - theory entries

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -20,7 +20,7 @@
         data() {
             return {
                 year: new Date().getFullYear(),
-                version: pkg.version || "0.9.6"
+                version: pkg.version || "0.9.7"
             }
         }
     }
